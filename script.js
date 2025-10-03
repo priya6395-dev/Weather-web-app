8f3fe0803caccdd5221244bb672177b3ea224446
priya6395-dev <priya.kindra6395@gmail.com>
2025-10-03 14:35:56 +0530

    Project completed — updated script.js

--- script.js
+++ script.js
@@ -237,4 +237,4 @@ navigator.geolocation.getCurrentPosition(position =>{
 searchBtn.addEventListener('click', getCityCoordinates);
 locationBtn.addEventListener('click', getUserCoordinates);
 cityInput.addEventListener('keyup', e => e.key === 'Enter' && getCityCoordinates());
-window.addEventListener('load', getUserCoordinates);           
+window.addEventListener('load', getUserCoordinates);    
